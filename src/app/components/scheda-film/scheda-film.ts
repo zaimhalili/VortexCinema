330119360417ba7a3885e9aecf522d9065f435d9
Diego De Luigi <diegolinda2018@gmail.com>
2026-02-16 10:09:50 +0100

    Creazione Scheda Film

---
+++ src/app/components/scheda-film/scheda-film.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-scheda-film',
+  imports: [],
+  templateUrl: './scheda-film.html',
+  styleUrl: './scheda-film.css',
+})
+export class SchedaFilm {
+
+}
