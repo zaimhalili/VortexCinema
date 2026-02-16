be3f558a4ac977b25ddec42b70bc0949e46174c0
Daniel Campana <daniel.campana@studenti.ittsrimini.edu.it>
2026-02-16 15:53:22 +0100

    Chi Siamo Page

---
+++ src/app/components/chi-siamo/chi-siamo.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-chi-siamo',
+  imports: [],
+  templateUrl: './chi-siamo.html',
+  styleUrl: './chi-siamo.css',
+})
+export class ChiSiamo {
+
+}
