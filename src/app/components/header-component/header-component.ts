b3cc50391117e416d5f9caf7936a63df901d36dd
Diego De Luigi <diegolinda2018@gmail.com>
2026-02-18 17:14:43 +0100

    Responsive header ( non finito )

--- src/app/components/header-component/header-component.ts
+++ src/app/components/header-component/header-component.ts
@@ -8,5 +8,9 @@ import { RouterLink, RouterLinkActive } from "@angular/router";
   styleUrl: './header-component.css',
 })
 export class HeaderComponent {
+  menuOpen = false;
 
+  toggleMenu() {
+    this.menuOpen = !this.menuOpen;
+  }
 }
