86cc570b1224bf5f3824cd2418f7aeca41fd806e
Zaim Halili <halilizaim@gmail.com>
2026-02-20 11:41:47 +0100

    installed AOS (Animations on Scroll)
    added animations to all the pages of the website

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,14 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
+import AOS from 'aos';
 
 bootstrapApplication(App, appConfig)
+  .then(() => {
+    AOS.init({
+      duration: 800,
+      once: true,
+      easing: 'ease-out-cubic'
+    });
+  })
   .catch((err) => console.error(err));
